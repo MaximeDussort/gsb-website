dd890c9f7b297dc16e07e744e80def65ca2be36a
Maxime Dussort <maxime.dussort@gmail.com>
2023-03-21 09:55:34 +0100

    Ajout page Liste Medicaments et finalisation de l'accueil

--- controle/Controle.php
+++ controle/Controle.php
@@ -2,4 +2,8 @@
     function chargementVuePrincipale(){
         require_once "vue/Accueil.php";
     }
+
+    function chargementVueMedocs(){
+        require_once "vue/ListMedicament.php";
+    }
 ?>
---
+++ vue/ListMedicament.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="vue/bootstrap.css">
+    <link rel="stylesheet" href="vue/style.css">
+    <title>GSB - Liste des Medicaments</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="text-center col-lg-12 col-sm-1">
+                <nav>
+                    <ul class="no-point">
+                        <li><strong><a href="index.php?action=LiMed">Liste&nbspdes&nbspmédicaments</a></strong></li>
+                        <li><strong><a href="">Liste&nbspdes&nbspactivités</a></strong></li>
+                        <li><strong><a href="">Mentions&nbspjuridiques</a></strong></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>     
+        <div class="row">
+            <div class="col-12 background-scientist">             
+            </div>
+            <div class="col-lg-10 col-sm-12 logo-text">
+                <a href="index.php">
+                    <img src="vue/images/logo-gsb.png" alt="logo_gsb">
+                </a>
+                <br><br>
+                <h1 style="color: white;"><strong>Liste des médicaments</strong></h1>
+                <br>
+                <table style="text-align: start;">
+                    <thead>
+                        <th>Nom</th>
+                        <th>Effets therapeutiques</th>
+                        <th>Effets secondaires</th>
+                        <th>Interactions entre medicaments</th>
+                        <th>Image</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                        
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
