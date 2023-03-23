4588759108d36ac8884741d65f197067d7b44e8a
Maxime Dussort <maxime.dussort@gmail.com>
2023-03-23 08:23:37 +0100

    Ajout complet de la page Mentions Juridique

--- vue/MentionsJuridiques.php
+++ vue/MentionsJuridiques.php
@@ -29,6 +29,44 @@
                 <br><br>
                 <h1 style="color: white;"><strong>Mentions Juridiques</strong></h1>
                 <br>
+                <div style="background-color: rgba(0, 0, 0, .5); padding: 1vh" class="couleur-mentions-juridiques">
+                    <p style="font-size: larger;" >Nous vous expliquons ici l'intérêt de la collection et du traitement de vos informations personnelles dans notre entreprise afin de vous expliquer et informer de son utilisation et de notre but.</p>
+                    <br>
+                    <p style="font-size: larger;"><strong>Pourquoi stockons-nous vos informations personnelles ?</strong></p>
+                    <p style="font-size: large;">Nous cherchons à alimenter, à utiliser et à traiter vos données pour connaître les activités les plus populaires dans notre entreprise dans le but d’améliorer nos futures activités.</p>
+                    <br>
+                    <p style="font-size: larger;"><strong>Quelles données collecterons-nous ?</strong></p>
+                    <p style="font-size: large;">Nous récupérons les informations que les utilisateurs ont remplies dans nos formulaires pour participer à une activité.</p>
+                    <br>
+                    <p style="font-size: larger;"><strong>Où vont mes données ?</strong></p>
+                    <p style="font-size: large;">Vos données personnelles nous permettent d’alimenter nos bases de données.</p>
+                    <br>
+                    <p style="font-size: larger;"><strong>Combien de temps mes données seront conservées ?</strong></p>
+                    <p style="font-size: large;">La durée maximale est de 2 ans. Cependant, la durée peut être déterminée selon l' objectif souhaité.</p>
+                    <br>
+                    <p style="font-size: larger;"><strong>Pourquoi nous utilisons les cookies ?</strong></p>
+                    <p style="font-size: large;">Les cookies nous permettent d’améliorer notre site internet.</p>
+                    <br>
+                    <p style="font-size: larger;"><strong>Est ce que mes données sont archiver ?</strong></p>
+                    <p style="font-size: large;">Les données personnelles qui ne sont plus utilisées pour atteindre notre objectif fixé seront conservées pour réaliser des statistiques.</p>
+                    <br>
+                    <p style="font-size: larger;"><strong>Que puis-je faire en tant qu’utilisateur ?</strong></p>
+                    <p style="font-size: large;">
+                        Vous pouvez demander au responsable de réaliser les actions suivante :<br>
+                        - Suppression de mes informations personnelles<br>
+                        - S’opposer au stockage de mon information personnelles<br>
+                        - Rectification de mes informations personnelles<br>
+                        - Récupération de mes informations personnelles<br>
+                        - Limitation du traitement de mes information personnelles<br>
+                        - Accés à mes données
+                    </p>
+                    <br>
+                    <p style="font-size: larger;"><strong>Partageons nous vos informations ?</strong></p>
+                    <p style="font-size: large;">Nous ne partageons pas vos informations avec les tiers, afin d'éviter les fuites des informations personnelles.</p>
+                </div>
+                <footer>
+                    <p>© Site web proposé par Maxime Dussort et Nicolas Kerpaal</p>
+                </footer>
             </div>
         </div>
     </div>
